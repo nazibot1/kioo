2fcf5d0ba4e36e6141240a32644240583edabe98
nazibot1 <42460638+nazibot1@users.noreply.github.com>
2018-11-01 15:50:16 +0300

    Add files via upload

---
+++ walt.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+
+const adminprefix = ".";
+const devs = ['465701100352176128','501162174350229511'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'avatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+});
+
+
+
+
+
+
+
+
+ 
+ 
+client.login('NTA2MDA2MjkwMTQ5OTMzMDU2.Drx7Yw.ce5MMpjC5YkTS7yZozFSJ-39Ng4');
+
